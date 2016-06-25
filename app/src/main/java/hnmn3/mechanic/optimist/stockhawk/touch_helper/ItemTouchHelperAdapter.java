0fc379bf47aaa8ce6d10b9632248f26d9f308407
Manish Menaria <Itss.manish.menaria@gmail.com>
2016-06-25 15:42:27 +0530

    Initial commit

---
+++ app/src/main/java/hnmn3/mechanic/optimist/stockhawk/touch_helper/ItemTouchHelperAdapter.java
@@ -0,0 +1,7 @@
+package hnmn3.mechanic.optimist.stockhawk.touch_helper;
+
+
+public interface ItemTouchHelperAdapter {
+
+  void onItemDismiss(int position);
+}
---
+++ app/src/main/java/hnmn3/mechanic/optimist/stockhawk/touch_helper/ItemTouchHelperViewHolder.java
@@ -0,0 +1,7 @@
+package hnmn3.mechanic.optimist.stockhawk.touch_helper;
+
+public interface ItemTouchHelperViewHolder {
+  void onItemSelected();
+
+  void onItemClear();
+}
